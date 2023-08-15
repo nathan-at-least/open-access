8461a98d1c4b6975a227f6e365d97cac631a6e81
Nate Wilcox <nathan+dev@electriccoin.co>
2023-08-15 13:43:32 +0900

    Integrate `tracing` support.

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,7 +1,17 @@
 use crate::cli::options::Options;
 
 /// Run the commandline tool; `open-access` executable directly delegates to this
+///
+/// This function initializes `tracing`, parses commandline options, and then runs them. An application wanting to embed `open-access` functionality should consider using [Options::run] instead.
 pub async fn run() -> anyhow::Result<()> {
+    init_tracing();
     let options = Options::parse();
+    tracing::debug!(?options);
     options.run().await
+}
+
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 }
